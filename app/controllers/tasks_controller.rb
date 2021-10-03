7ab6db08218a947e5cda904616f41c94f9390f3b
kazunari0813 <haru1529@icloud.com>
2021-10-03 01:54:32 +0000

    検証追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,9 +28,13 @@ class TasksController < ApplicationController
   end
   
   def create
-    task = Task.new(task_paramas)
-    task.save!
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+    @task = Task.new(task_paramas)
+    if @task.save
+      redirect_to @task, notice: "タスク「#{task.name}」を登録しました。"
+    else
+      render :new
+    end
+    
   end
   
   private
